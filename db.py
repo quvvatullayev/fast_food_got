843e8ac4b6a66069251a88f7bf7c8cd498019b6d
ogabek <quvvatullayev@gmail.com>
2023-07-21 19:25:20 +0500

    create get category list db

---
+++ db.py
@@ -0,0 +1,22 @@
+import requests
+import json
+from tinydb import TinyDB, Query
+from tinydb.table import Document
+
+class DB:
+    def __init__(self, path) -> None:
+        self.db = TinyDB(path)
+        self.query = Query()
+        self.base_url = "http://127.0.0.1:8000/"
+
+    def get_category_list(self):
+        request = requests.get(self.base_url+"category/list/")
+        if request.status_code == 200:
+            data = request.json()
+        else:
+            data = {"error":"request not fount"}
+        return data
+    
+
+db = DB('db.json')
+print(db.get_category_list())
