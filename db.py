e6734485aefd0b92f617a90c2fdf28a51a594c5d
ogabek <quvvatullayev@gmail.com>
2023-07-30 18:41:39 +0500

    create order db

--- db.py
+++ db.py
@@ -2,6 +2,7 @@ import requests
 import json
 from tinydb import TinyDB, Query
 from tinydb.table import Document
+import datetime
 
 class DB:
     def __init__(self, path) -> None:
@@ -56,7 +57,31 @@ class DB:
         else:
             data = {'error':'request not fount'}
         return data
+
+    def create_order(self, data:dict):
+        now = datetime.datetime.now()
+        hour = now.hour + 1
+        minute = now.minute
+        new_time = datetime.datetime(now.year, now.month, now.day, hour, minute)
+        data['hour'] = new_time
+        request = requests.post(self.base_url+"order/create/", data=data)
+        if request.status_code == 201:
+            data = request.json()
+        else:
+            data = {'error':'request not fount'}
+        return data
+        
     
 
 db = DB('db.json')
-print(db.get_product_detail(1))
+# data = {
+#     "name":"test",
+#     "phone":"123456789",
+#     "address":"test",
+#     "product":1,
+#     'count':1,
+# }
+
+
+# print(db.create_order(data))
+# # print(db.create_order(data))
